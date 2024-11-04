7828a5d9ab95e56a9aaed6527f8a24efb665258a
Makenchii <marwane422@hotmail.fr>
2024-11-04 17:57:19 +0100

    m

---
+++ index.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import cowsay from "cowsay";
+
+console.log(
+	cowsay.say({
+		text: `Hello, i am ${process.env.NAME},from ${process.env.CAMPUS}`,
+		e: "oO",
+		T: "U ",
+	}),
+);
